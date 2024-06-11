43b687255b0d2b1e10e7f72ef381e428316942b7
BryanR69 <berniramose10@gmail.com>
2024-06-10 23:15:13 -0500

    Add files via upload

---
+++ DisenaelsiguienteformularioenJavaFx/src/com/example/javafxcontrols/YourController.java
@@ -0,0 +1,36 @@
+package com.example.javafxcontrols;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class YourController {
+    @FXML
+    private Button button;
+    @FXML
+    private CheckBox checkBox;
+    @FXML
+    private Hyperlink hyperlink;
+    @FXML
+    private ToggleButton toggleButton;
+    @FXML
+    private RadioButton radioButton;
+    @FXML
+    private Label label;
+    @FXML
+    private TextField textField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private TextArea textArea;
+    @FXML
+    private ProgressIndicator progressIndicator;
+    @FXML
+    private ProgressBar progressBar;
+    @FXML
+    private Slider slider;
+
+    @FXML
+    private void initialize() {
+        // Add any necessary initialization here
+    }
+}
